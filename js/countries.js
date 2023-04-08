cadf7da6120c54b658395c613bb87b11c2a969ef
PIASH <pjr@pjrs-MacBook-Air.local>
2023-04-08 15:26:58 +0600

    flags added

--- js/countries.js
+++ js/countries.js
@@ -12,7 +12,7 @@ const displayCountries = countries =>{
 
     const countriesContainer = document.getElementById('countries-container');
     countries.forEach(country => {
-        console.log(country);
+        // console.log(country);
         const countryDiv = document.createElement('div');
         countryDiv.classList.add('country');
         countryDiv.innerHTML = `
@@ -20,11 +20,30 @@ const displayCountries = countries =>{
         <h4>Official Name: ${country.name.official}</h4>
         <p>Population: ${country.population}</p>
         <p>Capital: ${country.capital  ? country.capital[0] : 'No Capital'}</p>
+        <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
         
         `
         countriesContainer.appendChild(countryDiv);
     })
 }
 
+const loadCountryDetails = (code) => {
+    // https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    // console.log('get country detail' , code);
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetails(data[0]))
+}
+
+
+const displayCountryDetails = country => {
+    const countryDetail = document.getElementById('country-details');
+    countryDetail.innerHTML = `
+    <h2>Details: ${country.name.common}</h2>
+    <img src="${country.flags.png}">
+    `
+}
 
 loadCountries();
